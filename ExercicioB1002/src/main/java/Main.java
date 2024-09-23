5b9e9f520b587f60cd675df5cbf8b368e709bfbb
Cauan-Silva <anacauansantos@gmail.com>
2024-09-22 22:07:10 -0300

    1002

---
+++ ExercicioB1002/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            double a = scanner.nextDouble();
+            
+            double area = 3.14159 * Math.pow(a, 2);
+            
+            System.out.printf("A=%.4f%n", area);
+        }
+    }
+}
